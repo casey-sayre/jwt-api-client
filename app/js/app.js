a7a9d338e49ab436e824d992c4491e8cd65d5a38
Casey Sayre <csayre@turnaboutsystems.com>
2016-06-07 22:01:31 -0400

    advances things to ui-router hello world

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,11 @@
 'use strict';
 
-angular.module( 'ClientApp', [ 'ngMaterial' ] )
-  .controller('MainController', function($scope) {
-    $scope.name = 'testName';
+angular.module('ClientApp', ['ngMaterial', 'ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        template: '<h1>hello world</h1>'
+      });
   });
